220145c2bc0af5bd742319367e49e6742787bd85
Rohit <rr37044@gmail.com>
2023-09-30 23:04:54 +0530

    Completed 7

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./components/HomePage";
 import Navbar from "./components/Navbar";
 import UsersModule from "./components/UsersModule";
