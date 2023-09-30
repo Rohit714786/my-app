cdbb18fffbe7ab11ad384ea37ea2f5538935bdd2
Rohit <rr37044@gmail.com>
2023-09-30 22:46:07 +0530

    Completed 3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Homepage from "./HomePage";
-import Navbar from "./Navbar";
-import UsersModule from "./UsersModule";
-import PhotosModule from "./PhotosModule";
+import Homepage from "./components/HomePage";
+import Navbar from "./components/Navbar";
+import UsersModule from "./components/UsersModule";
+import PhotosModule from "./components/PhotosModule";
 import Bootstrap from "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
